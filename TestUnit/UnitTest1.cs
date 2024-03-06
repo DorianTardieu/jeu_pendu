dab6db1974003eef195f53a12b9076fd722df1f4
Neil Tujague <neiltujague@gmail.com>
2024-03-06 16:35:32 +0100

    Push Neil
    Modif de TestUnit avec différents test unitaire pour les variable dans la page principale

--- TestUnit/UnitTest1.cs
+++ TestUnit/UnitTest1.cs
@@ -6,19 +6,49 @@ namespace TestUnit
 {
     public class UnitTest1
     {
-        [Fact]
-        public void PassingTest()
+        [Test]
+        public void TestMotChoisiNotNullOrEmpty()
         {
-            Assert.Equal(4, 2 + 2);
+            //Verifi que on a bien choisis un mot
+            MainWindow mainWindow = new MainWindow();
+            Assert.That(!string.IsNullOrEmpty(mainWindow.motChoisi));
         }
 
-        [Fact]
-        public void FailingTest()
+        [Test]
+        public void TestMotCacheInitializedWithAsterisks()
         {
-            Assert.Equal(5, 2 + 2);
+            // Verifi la longueur du mot
+            MainWindow mainWindow = new MainWindow();
+            string expected = new string('*', mainWindow.motChoisi.Length);
+            Assert.AreEqual(expected, mainWindow.motCache);
         }
 
+        [Test]
+        public void TestNombreVieInitializedWithSeven()
+        {
+            //Verifi le nombre de vies 
+            MainWindow mainWindow = new MainWindow();
+            Assert.AreEqual(7, mainWindow.nombreVie);
+        }
 
-       
+        [Test]
+        public void TestTimerTicks()
+        {
+            //Verifi que le timer est plus grand que 0
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Timer(null, null);
+            Assert.Greater(mainWindow.secondes, 0);
+        }
+
+        [Test]
+        public void TestBoutonClickCorrectGuess()
+        {
+            //Verifi l'algo avec un bon mot
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.motChoisi = "TEST";
+            mainWindow.tbDisplay.Text = "****";
+            mainWindow.BoutonClick(mainWindow.btnT, null);
+            Assert.AreEqual("T***", mainWindow.tbDisplay.Text);
+        }
     }
 }
