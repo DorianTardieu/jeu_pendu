180d6ae7b32feab2d2d51f8a78cac518bfaf33db
DorianTardieu <dorian.tardieu@saintmichelannecy.fr>
2024-03-06 16:37:38 +0100

    modif testunitaire
    Test unitaire des variable du code MainWindows.cs

--- TestUnit/UnitTest1.cs
+++ TestUnit/UnitTest1.cs
@@ -6,19 +6,53 @@ namespace TestUnit
 {
     public class UnitTest1
     {
-        [Fact]
-        public void PassingTest()
+        [TestMethod]
+        public void Constructor_InitializesVariablesCorrectly()
         {
-            Assert.Equal(4, 2 + 2);
+         
+            MainWindow mainWindow = new MainWindow();
+
+            // Assert
+            Assert.IsNotNull(mainWindow.motChoisi); //Verifie que mot choisi n'est pas null
+            Assert.AreEqual(7, mainWindow.nombreVie);//Verifie que le nombre de vie n'est pas null
+            Assert.AreEqual(0, mainWindow.minutes);//Verifie que le temps en minute est égale a 0 (pour le depart)
+            Assert.AreEqual(0, mainWindow.secondes);//Verifie que le temps en seconde est égale a 0 (pour le depart)
+            Assert.IsNotNull(mainWindow.nombreAleatoire);//Verifie que le nombre aleatoire pour choir dans la listes n'est pas null
+            Assert.IsNotNull(mainWindow.listeMots);//Verifie que la listesmots n'est pas null
+            Assert.IsNotNull(mainWindow.chrono);//Verifie que le chrono est pas null
+            Assert.IsNotNull(mainWindow.motCache);//Verifie que mot caché n'est pas null
+            Assert.IsFalse(string.IsNullOrEmpty(mainWindow.motCache));//Verifie que mot cache n'est pas encore trouvé
         }
 
-        [Fact]
-        public void FailingTest()
+        [TestMethod]
+        public void Timer_IncrementsSecondsCorrectly()
         {
-            Assert.Equal(5, 2 + 2);
+            //verifie que le timer fonctionne
+          
+            MainWindow mainWindow = new MainWindow();
+
+           
+            mainWindow.Timer(null, EventArgs.Empty);
+
+         
+            Assert.AreEqual(1, mainWindow.secondes);
+        }
+        [Test]
+        public void TestBoutonClickIncorrectGuess()
+        {
+             // verifie l'algorithme en testant quand le mot est faux
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.motChoisi = "TEST";
+            mainWindow.tbDisplay.Text = "****";
+
+            
+            mainWindow.BoutonClick(mainWindow.btnA, null);
+
+            Assert.AreEqual("****", mainWindow.tbDisplay.Text);
+            Assert.AreEqual(6, mainWindow.nombreVie);
         }
 
 
-       
+
     }
 }
